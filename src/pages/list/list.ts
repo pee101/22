86195e7ce454902fe714594995309aa2d2db1b62
pee101 <32015770+pee101@users.noreply.github.com>
2018-08-02 12:12:01 +0700

    Add files via upload

---
+++ src/pages/list/list.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import{HomePage} from'../home/home';
+@Component({
+  selector: 'page-list',
+  templateUrl: 'list.html'
+})
+export class ListPage {
+  selectedItem: any;
+  icons: string[];
+  items: Array<{title: string, note: string, icon: string}>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+   
+  }
+
+  Onclickpop(){
+
+  this.navCtrl.pop();
+
+  }
+}
